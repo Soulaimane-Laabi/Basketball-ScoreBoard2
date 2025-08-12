f051b11c558ab8688711f9fdc5b86f736d0267f6
Soulaimane-Laabi <laabi7551@gmail.com>
2025-08-12 16:31:29 +0100

    commit initial files

---
+++ script.js
@@ -0,0 +1,47 @@
+let tangierEl = document.getElementById("tangier-el");
+let tetouanEl = document.getElementById("tetouan-el");
+let tangierPoints = 0;
+let tetouanPoints = 0;
+
+function tangierOne() {
+    tangierPoints += 1
+    tangierEl.innerText = tangierPoints
+}
+
+function tangierTwo() {
+    tangierPoints += 2
+    tangierEl.innerText = tangierPoints
+}
+
+function tangierThree() {
+    tangierPoints += 3
+    tangierEl.innerText = tangierPoints
+}
+
+function tangierMinus() {
+    tangierPoints -= 1
+    tangierEl.innerText = tangierPoints
+}
+
+
+
+function tetouanOne() {
+    tetouanPoints += 1
+    tetouanEl.innerText = tetouanPoints
+}
+
+function tetouanTwo() {
+    tetouanPoints += 2
+    tetouanEl.innerText = tetouanPoints
+}
+
+function tetouanThree() {
+    tetouanPoints += 3
+    tetouanEl.innerText = tetouanPoints
+}
+
+function tetouanMinus() {
+    tetouanPoints -= 1
+    tetouanEl.innerText = tetouanPoints
+}
+
